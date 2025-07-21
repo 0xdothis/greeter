ce36406315123f9eaa542672e9775a7321588bcd
0xdothis <olagokesikiru06@gmail.com>
2025-07-21 23:51:00 +0100

    feat: fix error state

--- src/App.js
+++ src/App.js
@@ -10,13 +10,17 @@ function App() {
   const [text, setText] = useState("");
   const [address, setAddress] = useState("");
   const [result, setResult] = useState("");
-  let signer;
-  let provider = new ethers.BrowserProvider(window.ethereum);
+  const [error, setError] = useState("");
+
+  async function requestAccount() {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+  }
 
   async function connectWallet() {
     if (window.ethereum) {
       await window.ethereum.request({ method: "eth_requestAccounts" });
-      provider = signer = await provider.getSigner();
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      const signer = await provider.getSigner();
       const addy = `${signer.address.slice(0, 5)}.....${signer.address.slice(
         -5
       )}`;
@@ -32,69 +36,81 @@ function App() {
   const handleSet = async () => {
     try {
       if (!text) {
-        alert("Please enter a message before setting.");
+        setError("Please enter a message before setting.");
+        setResult("");
         return;
       }
 
-      signer = await provider.getSigner();
-      const addy = `${signer.address.slice(0, 5)}.....${signer.address.slice(
-        -5
-      )}`;
+      if (window.ethereum) {
+        await requestAccount();
+        const provider = new ethers.BrowserProvider(window.ethereum);
+        const signer = await provider.getSigner();
+        const contract = new ethers.Contract(contractAddress, abi, signer);
 
-      setAddress(addy);
-      const contract = new ethers.Contract(contractAddress, abi, signer);
-      const tx = await contract.setMessage(text);
-      const txReceipt = await tx.wait();
-      console.log("Transaction successful:", txReceipt);
-
-      setText("");
+        const tx = await contract.setMessage(text);
+        const txReceipt = await tx.wait();
+        console.log("Transaction successful:", txReceipt);
+        setText("");
+      } else {
+        setError(
+          "MetaMask not found. Please install MetaMask to use this application."
+        );
+      }
     } catch (error) {
-      console.error("Error setting message:", error);
-      alert(error.message || error);
+      setError(`Error setting message: ${error}`);
     }
   };
 
-  async function handleGet() {
+  const handleGet = async () => {
     try {
-      signer = await provider.getSigner();
-      const addy = `${signer.address.slice(0, 5)}.....${signer.address.slice(
-        -5
-      )}`;
+      if (window.ethereum) {
+        const provider = new ethers.BrowserProvider(window.ethereum);
 
-      setAddress(addy);
-      const contract = new ethers.Contract(contractAddress, abi, signer);
-      const tx = await contract.getMessage();
-      const txReceipt = await tx;
-      setResult(txReceipt);
-      console.log("Transaction get:", txReceipt);
-    } catch (err) {
-      console.error("Error setting message:", err);
-      alert(err.message || err);
+        const contract = new ethers.Contract(contractAddress, abi, provider);
+
+        const tx = await contract.getMessage();
+
+        setResult(tx);
+        console.log("Transaction successful:", tx);
+        setError("");
+      } else {
+        setError(
+          "MetaMask not found. Please install MetaMask to use this application."
+        );
+      }
+    } catch (error) {
+      setError(`Error setting message: ${error.message}`);
     }
-  }
+  };
 
   return (
     <div className="wrapper">
       <button onClick={connectWallet} className="btn">
         {address ? address : "Connect wallet"}
       </button>
-      <h1 className="heading">Set Message on Smart Contract</h1>
-      <input
-        className="text"
-        type="text"
-        placeholder="Set message"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
-      {!!result && <p className="result">{result}</p>}
-      <div>
-        <button className="btn" onClick={handleSet}>
-          Set Message
-        </button>
-        <button className="btn" onClick={handleGet}>
-          Get Message
-        </button>
-      </div>
+      {address && (
+        <>
+          <h1 className="heading">Set Message on Smart Contract</h1>
+          <input
+            className="text"
+            type="text"
+            placeholder="Set message"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          {result && <p className="result">{result}</p>}
+          {error && <p className="result">{error}</p>}
+
+          <div>
+            <button className="btn" onClick={handleSet}>
+              Set Message
+            </button>
+            <button className="btn" onClick={handleGet}>
+              Get Message
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
